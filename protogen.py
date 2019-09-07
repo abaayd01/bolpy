adbe7693e674020cb0e4534e02ace52e49977695
Ayden <ayden@codefishstudio.com>
2019-09-07 22:36:01 +0930

    update file path for protogen

--- protogen.py
+++ protogen.py
@@ -1,11 +1,11 @@
 import os
 
-PROTO_FILES_PATH = "../proto_files"
+PROTO_FILES_PATH = "../bolproto"
 mkdirCmd = f'mkdir -p {PROTO_FILES_PATH}/bolpy/proto'
 os.system(mkdirCmd)
 
 copyBolProtoCmd = f'cp {PROTO_FILES_PATH}/bol.proto {PROTO_FILES_PATH}/bolpy/proto'
 os.system(copyBolProtoCmd)
 
-protogenCmd = 'python -m grpc_tools.protoc -I=../proto_files --python_out=. --grpc_python_out=. ../proto_files/bolpy/proto/bol.proto'
+protogenCmd = 'python -m grpc_tools.protoc -I=../bolproto --python_out=. --grpc_python_out=. ../bolproto/bolpy/proto/bol.proto'
 os.system(protogenCmd)
